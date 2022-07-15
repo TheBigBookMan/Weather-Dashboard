ce617560f4bb7728c5981836d6591f97d5a3db8a
Ben Smerd <bsmerd@adam.com.au>
2022-07-15 21:41:18 +0930

    Created the functionality to store city searches into the local storage and retrieve them in the history section.

--- app.js
+++ app.js
@@ -3,68 +3,99 @@
 const inputSelection = document.getElementById('search-city');
 const submitBtn = document.getElementById('submit-btn');
 const historyList = document.querySelector('.search-history-list');
-
+var savedLocalCities = [];
+var savedStorage;
+var newLi;
+var newLiButton;
 var url;
+
+
+
+// HAVE ARRAY OF THE PREVIOUS SESSION THAT HAS THE LOCAL STORAGE COME UP THROUGH INIT AND THEN USE THAT ARRAY TO THEN CREATE A NEW ONE WHICH THEN GETS SAVED AND SENT IN TO THE NEXT SESSION LIKE A LOOP
 
 // EVENT LISTENER FOR THE SEARCH BAR LINKED TO THE SUBMIT BUTTON
 // NEED TO INPUT THE FUNCTION NAME AND PASS THE PARAMETER FROM THE SEARCH-CITY VALUE
-submitBtn.addEventListener('click', function(){
+submitBtn.addEventListener('click', event =>{
     event.preventDefault();
     var input = inputSelection.value;
-    console.log(input)
-    localStorage.setItem("inputCity", input);
-    getHistory()
+    if (input === "") {
+        alert("Please enter a valid city.")
+        return;
+    }
     url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=3&appid=d9b9bc1832593b46ab69e998211f9b08`;
-    console.log(url)
-    getApiLatLon();// HAVE PARAMETER AS INPUT
+    // console.log(url)
+    getApiLatLon(input);
 });
 
 
 // FUNCTION THAT RETRIEVES THE SEARCHES FROM THE LOCAL STORAGE
-const getHistory = () => {
-    var outputCity = localStorage.getItem("inputCity");
-    createHistoryList(outputCity)
-    console.log(outputCity)
-}
 
 // FUNCTION THAT STORES THE SEARCH HISTORY SELETIONS IN THE DIV UNDER SEARCH BAR
-const createHistoryList = cityHistory => {
-    var newLi = document.createElement('li');
-    var newLiButton = document.createElement('button');
+const createHistoryList = (input) => {
+    newLi = document.createElement('li');
+    newLiButton = document.createElement('button');
     newLi.appendChild(newLiButton);
     newLiButton.className = "search-history-item"
-    newLiButton.textContent = cityHistory;
-    
     historyList.appendChild(newLi);
+    newLiButton.textContent = input
 }
+
+const init = () => {
+    savedLocalCities = JSON.parse(localStorage.getItem("inputCityArray"))
+    savedStorage = savedLocalCities
+    if (savedLocalCities === null) {
+        return;
+    } else {
+        for (var i = 0; i < savedLocalCities.length; i++) {
+            newLi = document.createElement('li');
+            newLiButton = document.createElement('button');
+            newLi.appendChild(newLiButton);
+            newLiButton.className = "search-history-item"
+            historyList.appendChild(newLi);
+            newLiButton.textContent = savedLocalCities[i]
+        };
+    }
+    
+};
+
+// ADD EVENT LISTENER TO THE HISTORY LIST BUTTONS SO THEY CALL THE API FUNCTION TO GET THE INFORMATION
+
+
+// FUNCTION TO SET ITEM TO LOCAL STORAGE AND THEN GET HISTORY FUNCTION
+const storeLocally = input => {
+    if (savedStorage === null) {
+        savedStorage = [input]
+        localStorage.setItem("inputCityArray", JSON.stringify(savedStorage));
+        createHistoryList(input)
+        return;
+    } else {
+        savedStorage.push(input);
+        localStorage.setItem("inputCityArray", JSON.stringify(savedStorage));
+    }
+    createHistoryList(input)
+}
+
 
 
 // FUNCTION THAT GETS THE API FROM THE WEATHER DATABSE FOR GEOCODER AND RETURNS THE LAT AND LONG INTO THE OTHER FUNCTION THAT TURNS IT INTO ONECALL INFO
-const getApiLatLon = () => { // HAVE ARGUMENT AS cityChosen
+const getApiLatLon = input => { // HAVE ARGUMENT AS cityChosen
     fetch(url)
         .then(function(response) {
-            console.log(response)
-            console.log(response.ok)
             return response.json();
         })
         .then(function(data) {
-            if(data.length === 0) {
-                alert("Please Insert Valid City")
-                // MAYBE ADD REMOVE FROM LOCAL STORAGE AS WELL BECAUSE DONT WANT HISTORY OF ONE THAT WAS INVALID
-            }
-            console.log(data)
+            if(data.length > 0) {
+                storeLocally(input);
+            } else {
+                alert("Please Insert Valid City");
+            };
+            // console.log(data)
         })
 };
     
 
 
 // NEED A FUNCTION TAHT TAKES IN THE LAT AND LONG FROM GEOCODER API AND THEN TRANSFORMS THAT INTO THE REQUEST FOR THE WEATHER INFORMATION FOR THE ONECALL API REQUEST
-
-
-
-
-// FUNCTION THAT STORES THE SEARCH RESULT INTO THE LOCAL STORAGE
-
 
 
 // FUNCTION THAT RETREIVES THE API SEARCH INTO THE MAIN SECTION
@@ -76,3 +107,4 @@ const getApiLatLon = () => { // HAVE ARGUMENT AS cityChosen
 
 
 // FUNCTION THAT PRINTS OUT CARDS OF THE FIVE DAYS WITH INFORMATION ON THEM
+init()
