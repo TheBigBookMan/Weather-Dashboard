cf1343eca48f0e8e5ef9b5af84f8420ec7cd8f80
Ben Smerd <bsmerd@adam.com.au>
2022-07-16 22:54:41 +0930

    Cleaned up the Javascript code.

--- app.js
+++ app.js
@@ -12,44 +12,42 @@ var urlWeather;
 var urlIcon;
 
 // Result section
-var resultContainer = document.querySelector('.result-container')
-var resultTitle = document.getElementById('result-city-title')
-var resultDate = document.getElementById('result-city-date')
-var resultIcon = document.getElementById('result-city-icon')
-var resultList = document.querySelector('.result-city-info-list')
+var resultContainer = document.querySelector('.result-container');
+var resultTitle = document.getElementById('result-city-title');
+var resultDate = document.getElementById('result-city-date');
+var resultIcon = document.getElementById('result-city-icon');
+var resultList = document.querySelector('.result-city-info-list');
 var dateTime;
 var dateUnix;
-var futureCards = document.querySelector('.future-day-cards')
+var futureCards = document.querySelector('.future-day-cards');
 
-// EVENT LISTENER FOR THE SEARCH BAR LINKED TO THE SUBMIT BUTTON
-// NEED TO INPUT THE FUNCTION NAME AND PASS THE PARAMETER FROM THE SEARCH-CITY VALUE
+// Event listener for the submit button to submit the search City name into the GeoCoder url, and then calls the API function
 submitBtn.addEventListener('click', event =>{
     event.preventDefault();
     var input = inputSelection.value;
     if (input === "") {
-        alert("Please enter a valid city.")
+        alert("Please enter a valid city.");
         return;
-    }
+    };
     urlGeoCode = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=3&appid=d9b9bc1832593b46ab69e998211f9b08`;
     getApiLatLon(input);
 });
 
-
-// FUNCTION THAT STORES THE SEARCH HISTORY SELETIONS IN THE DIV UNDER SEARCH BAR
+// Function that creates the buttons of history cities typed in
 const createHistoryList = (input) => {
     newLi = document.createElement('li');
     newLiButton = document.createElement('button');
     newLi.appendChild(newLiButton);
-    newLiButton.className = "search-history-item"
+    newLiButton.className = "search-history-item";
     historyList.appendChild(newLi);
-    newLiButton.textContent = input
-    createBtnEvent(input)
-}
+    newLiButton.textContent = input;
+    createBtnEvent(input);
+};
 
-// Initialization function that creates the history section on refresh page
+// Initialization function that creates the history section on refresh page by using a for loop to iterate through the local storage array of stored city names
 const init = () => {
-    savedLocalCities = JSON.parse(localStorage.getItem("inputCityArray"))
-    savedStorage = savedLocalCities
+    savedLocalCities = JSON.parse(localStorage.getItem("inputCityArray"));
+    savedStorage = savedLocalCities;
     if (savedLocalCities === null) {
         return;
     } else {
@@ -57,32 +55,29 @@ const init = () => {
             newLi = document.createElement('li');
             newLiButton = document.createElement('button');
             newLi.appendChild(newLiButton);
-            newLiButton.className = "search-history-item"
+            newLiButton.className = "search-history-item";
             historyList.appendChild(newLi);
-            newLiButton.textContent = savedLocalCities[i]
-            createBtnEvent(savedLocalCities[i])
+            newLiButton.textContent = savedLocalCities[i];
+            createBtnEvent(savedLocalCities[i]);
         };
-    }
-    
+    };  
 };
 
-// ADD EVENT LISTENER TO THE HISTORY LIST BUTTONS SO THEY CALL THE API FUNCTION TO GET THE INFORMATION
+// Function that adds an event listener to the history selected buttons, which calls the API function
 const createBtnEvent = (input) => {
     newLiButton.addEventListener('click', event => {
-        event.preventDefault()
+        event.preventDefault();
         urlGeoCode = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=3&appid=d9b9bc1832593b46ab69e998211f9b08`;
-        getApiLatLon(input)
-    })
-}
+        getApiLatLon(input);
+    });
+};
 
-
-// PUT IF ELSE STATEMENT BEFORE THE CREATEHISTORY TO CHECK IF ITS ALREADY IN HISTORY SECTION TO STOP IT FROM CREATING ANOTHER BUTTON
-// FUNCTION TO SET ITEM TO LOCAL STORAGE AND THEN GET HISTORY FUNCTION
+// Function that takes in the city named and saves it to the local storage into an array, which then calls to create the history saved list
 const storeLocally = input => {
     if (savedStorage === null) {
-        savedStorage = [input]
+        savedStorage = [input];
         localStorage.setItem("inputCityArray", JSON.stringify(savedStorage));
-        createHistoryList(input)
+        createHistoryList(input);
         return;
     } else if (savedStorage.includes(input)) {
         return;
@@ -90,13 +85,11 @@ const storeLocally = input => {
         savedStorage.push(input);
         localStorage.setItem("inputCityArray", JSON.stringify(savedStorage));
     }
-    createHistoryList(input)
-}
+    createHistoryList(input);
+};
 
-
-
-// FUNCTION THAT GETS THE API FROM THE WEATHER DATABSE FOR GEOCODER AND RETURNS THE LAT AND LONG INTO THE OTHER FUNCTION THAT TURNS IT INTO ONECALL INFO
-const getApiLatLon = input => { // HAVE ARGUMENT AS cityChosen
+// Function that receives the city name input and then fetches the GeoCode API, which then returns the latitude and longitude, which then calls the API weather function
+const getApiLatLon = input => {
     fetch(urlGeoCode)
         .then(function(response) {
             return response.json();
@@ -107,142 +100,122 @@ const getApiLatLon = input => { // HAVE ARGUMENT AS cityChosen
             } else {
                 alert("Please Insert Valid City");
             };
-            var latGeoCode = data[0].lat
-            var lonGeoCode = data[0].lon
-            getApiWeather(latGeoCode, lonGeoCode, input)
-        })
+            var latGeoCode = data[0].lat;
+            var lonGeoCode = data[0].lon;
+            getApiWeather(latGeoCode, lonGeoCode, input);
+        });
 };
 
-// NEED A FUNCTION TAHT TAKES IN THE LAT AND LONG FROM GEOCODER API AND THEN TRANSFORMS THAT INTO THE REQUEST FOR THE WEATHER INFORMATION FOR THE ONECALL API REQUEST
-//NEED TEMP= temp, WIND=wind_speed + "km/h", HUMIDITY= humidity + %, UV INDEX = uvi (add in an if statement for color coding 0-2 green, 2-5 orange, 5+ red)
-
+// Function that takes in the latitude, longitude and input city name and fetches the one call API, which then returns the information about the weather for current day
 const getApiWeather = (lat, lon, input) => {
-    urlWeather = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=cb187059a60a4d1624b2d6be95ebcdf1`
+    urlWeather = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=cb187059a60a4d1624b2d6be95ebcdf1`;
     fetch(urlWeather)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            var currentTemp = data.current.temp
-            var currentWind = data.current.wind_speed
-            var currentHumidity = data.current.humidity
-            var currentUvi = data.current.uvi
-            var currentIcon = data.current.weather[0].icon
-            currentDayResult(currentTemp, currentWind, currentHumidity, currentUvi, input, currentIcon)
-            createFiveDayResults(data)
-        })
+            var currentTemp = data.current.temp;
+            var currentWind = data.current.wind_speed;
+            var currentHumidity = data.current.humidity;
+            var currentUvi = data.current.uvi;
+            var currentIcon = data.current.weather[0].icon;
+            currentDayResult(currentTemp, currentWind, currentHumidity, currentUvi, input, currentIcon);
+            createFiveDayResults(data);
+        });
         return;
-}
+};
 
-var resultListTemp = document.createElement('li')
-var resultListWind = document.createElement('li')
-var resultListHumidity = document.createElement('li')
-var resultListUvi = document.createElement('li')
-var resultListUviNum = document.createElement('span')
-var resultListIcon = document.createElement('img')
+var resultListTemp = document.createElement('li');
+var resultListWind = document.createElement('li');
+var resultListHumidity = document.createElement('li');
+var resultListUvi = document.createElement('li');
+var resultListUviNum = document.createElement('span');
+var resultListIcon = document.createElement('img');
 
-// FUNCTION THAT RETREIVES THE API SEARCH INTO THE MAIN SECTION
+// Function that takes in the information of the weather and then creates the current day information onto the webpage through Javascript DOM
 const currentDayResult = (temp, wind, humidity, uvi, input, icon) => {
-    resultTitle.textContent = input + "-"
+    resultTitle.textContent = input + "-";
     dateTime = moment();
     resultDate.textContent = dateTime.format("dddd, MMMM Do Y");
 
-    resultListIcon.className = 'icon-img'
-    resultListIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`  
-    resultIcon.append(resultListIcon)
-    //ADD IN ICON AFTER THE DATE
+    resultListIcon.className = 'icon-img';
+    resultListIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    resultIcon.append(resultListIcon);
     
     resultListTemp.textContent = "Temp: " + temp + "°C";
-    resultList.append(resultListTemp)
+    resultList.append(resultListTemp);
     
     resultListWind.textContent = "Wind: " + wind + " km/h";
-    resultList.append(resultListWind)
+    resultList.append(resultListWind);
 
-    resultListHumidity.textContent = "Humidity: " + humidity + "%"
-    resultList.append(resultListHumidity)
+    resultListHumidity.textContent = "Humidity: " + humidity + "%";
+    resultList.append(resultListHumidity);
 
-    resultListUvi.textContent = "UV Index: "
+    resultListUvi.textContent = "UV Index: ";
     if (uvi >= 0 && uvi < 2) {
-        resultListUviNum.textContent = uvi
-        resultListUviNum.className = 'color-code-green'
-        resultListUvi.append(resultListUviNum)
+        resultListUviNum.textContent = uvi;
+        resultListUviNum.className = 'color-code-green';
+        resultListUvi.append(resultListUviNum);
     } else if (uvi >= 2 && uvi < 5) {
-        resultListUviNum.textContent = uvi
-        resultListUviNum.className = 'color-code-orange'
-        resultListUvi.append(resultListUviNum)
+        resultListUviNum.textContent = uvi;
+        resultListUviNum.className = 'color-code-orange';
+        resultListUvi.append(resultListUviNum);
     } else if (uvi >= 5) {
-        resultListUviNum.textContent = uvi
-        resultListUviNum.className = 'color-code-red'
-        resultListUvi.append(resultListUviNum)
+        resultListUviNum.textContent = uvi;
+        resultListUviNum.className = 'color-code-red';
+        resultListUvi.append(resultListUviNum);
     }
-    resultList.append(resultListUvi)
+    resultList.append(resultListUvi);
     return;
-}
+};
 
-// FUNCTION THAT USES THE API DATA FOR THE NEXT FIVE DAYS 
+// Function that iterates through the data retrieved from the fetch function and finds the relevant data for the next 5 days
 const createFiveDayResults = data => {
-    console.log(data)
-    var dailyData = data.daily 
-    console.log(dailyData)
+    var dailyData = data.daily;
     var dailyTemp;
     var dailyWind;
     var dailyHumidity;
     var dailyUnix;
-    futureCards.textContent = ''
+    futureCards.textContent = '';
     for (var i = 1; i < 6; i++) {
-        dailyTemp = dailyData[i].temp.day
-        dailyWind = dailyData[i].wind_speed
-        dailyHumidity = dailyData[i].humidity
-        dailyUnix = dailyData[i].dt
-        dailyIcon = dailyData[i].weather[0].icon
-        console.log(dailyIcon)
-        printCardFiveDay(dailyUnix, dailyTemp, dailyWind, dailyHumidity, dailyIcon)
-    }
-}
+        dailyTemp = dailyData[i].temp.day;
+        dailyWind = dailyData[i].wind_speed;
+        dailyHumidity = dailyData[i].humidity;
+        dailyUnix = dailyData[i].dt;
+        dailyIcon = dailyData[i].weather[0].icon;
+        printCardFiveDay(dailyUnix, dailyTemp, dailyWind, dailyHumidity, dailyIcon);
+    };
+};
 
 
-// FUNCTION THAT PRINTS OUT CARDS OF THE FIVE DAYS WITH INFORMATION ON THEM
+// Function that takes in the weather information for the next 5 day cards and creates them through Javascript DOM
 const printCardFiveDay = (unix, temp, wind, humidity, icon) => {
+    var card = document.createElement('div');
+    card.className = 'future-card';
+    futureCards.append(card);
     
-    var card = document.createElement('div')
-    card.className = 'future-card'
-    futureCards.append(card)
-    
+    var cardDate = document.createElement('h3');
+    dateUnix = moment.unix(unix).format("DD/MM/Y");
+    cardDate.textContent = dateUnix;
+    card.append(cardDate);
 
-    var cardDate = document.createElement('h3')
-    dateUnix = moment.unix(unix).format("DD/MM/Y")
-    cardDate.textContent = dateUnix
-    card.append(cardDate)
+    var cardIcon = document.createElement('img');
+    cardIcon.className = 'icon-img';
+    cardIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    card.append(cardIcon);
 
-    var cardIcon = document.createElement('img')
-    cardIcon.className = 'icon-img'
-    cardIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png` 
-    card.append(cardIcon)
+    var cardTemp = document.createElement('p');
+    cardTemp.textContent = 'Temp: ' + temp + '°C';
+    card.append(cardTemp);
 
-    var cardTemp = document.createElement('p')
-    cardTemp.textContent = 'Temp: ' + temp + '°C'
-    card.append(cardTemp)
+    var cardWind = document.createElement('p');
+    cardWind.textContent = 'Wind: ' + wind + ' km/h';
+    card.append(cardWind);
 
-    var cardWind = document.createElement('p')
-    cardWind.textContent = 'Wind: ' + wind + ' km/h'
-    card.append(cardWind)
+    var cardHumidity = document.createElement('p');
+    cardHumidity.textContent = 'Humidity: ' + humidity + '%';
+    card.append(cardHumidity);
+};
 
-    var cardHumidity = document.createElement('p')
-    cardHumidity.textContent = 'Humidity: ' + humidity + '%'
-    card.append(cardHumidity)
-}
-
-
-
-// GET API ICONS FOR THE CLOUD, SUN ETC
-
-
-
-// FUNCTION THAT USES THE API DATA TO INPRINT OUT THE DAY WEATHER AND IT"S CHARACTERSITICS
-
-
-
-
-
-
+// Calling the initialization function to load the history city choices
 init()
